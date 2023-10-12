c0b8ba5e11eae36f887f14431607fde37c94f999
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-10-12 10:40:29 -0400

    Update VariableExample.java

--- VariableExample.java
+++ VariableExample.java
@@ -1,24 +1,25 @@
-// This is a integer
-int num = 55;
+// This is a class, we will cover these later
+class Main {
+  // This is a integer
+  int num = 55;
+  // This is a double
+  double decimalNum = 44.54;
+  // This is a float
+  float decimalNum2 = 5.135;
+  // This is a boolean
+  boolean tValue = true;
+  // Or
+  boolean fValue = false;
+  // This is a String
+  String exampleStr = "This is a string";
+  // You can change the value of the variables as well,
+  decimalNum = .57;
+  // You can also do this
+  int example;
 
-// This is a double
-double decimalNum = 44.54;
+  // This is a method we will cover these later
+  public static void main(String[] args) {
+    example = num;
+  }
+}
 
-// This is a float
-float decimalNum2 = 5.135;
-
-// This is a boolean
-boolean tValue = true;
-
-// Or
-boolean fValue = false;
-
-// This is a String
-String exampleStr = "This is a string";
-
-// You can change the value of the variables as well,
-decimalNum = .57;
-
-// You can also do this
-int example;
-example = num;
