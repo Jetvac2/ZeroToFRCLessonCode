f3774c0e41edfeb7e6be616a34cf76d8eacbd3d3
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-10-12 10:37:48 -0400

    Update ifStatementsAndBooleans.java

--- ifStatementsAndBooleans.java
+++ ifStatementsAndBooleans.java
@@ -1,1 +1,48 @@
+// Operators
+// Greater than: >
+// Greater than or equal to: >=
+// Less than: <
+// Less than or equal to: <=
+// Equal to: ==
+// Not equal to: !=
+// And: &&
+// Or: ||
+// Not: !
+class Main {
+      boolean example = 33 < 57;
+    public static void main(String[] args)
+    {
+        // Example "if" statement
+        if(true) {
+          // Code in here will run
+        }
+      
+        // Example "if" statement with boolean equation
+        if((56 >= 23) && (85 == 22)) {
+          // Code in here will not run
+        } else if((22 != 22) || (82 < 100)) {
+          // Code in here will not run
+        } else if((55 != 1) || (1 >= 55)) {
+          // Code in here will run
+        }
+      
+        // Example "if" statement with boolean equation
+        if((!(56 <= 23)) && (85 == 22)) {
+            // Code in here will not run
+        } else if((22 != 22) || (82 < 100)) {
+          // Code in here will not run
+        } else {
+          // Code in here will run
+        }
+        
+        if(example) {
+          // Code in here will run
+        }
+
+      // Example print to console
+      System.out.println("The answere is:" + !example);
+    }
+}
+
+
 
