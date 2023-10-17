08820c980c298b2e53512ebdcd0ae21278f5ec55
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-10-17 10:34:01 -0400

    Update methodChallengeSolution.java

--- methodChallengeSolution.java
+++ methodChallengeSolution.java
@@ -1,1 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+      sum = add(55, number);
+      System.out.println(sum);
+      printNumber(sum);
+    }
 
+    // type, name, (arguments)
+    int add(double base, double height) {
+      return num1 + num2;
+    }
+}
