586d121470cc37656a0a819771bbe310d2e1c777
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-10-17 10:56:28 -0400

    Create methodSolution.java

---
+++ methodSolution.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+      area = triangleArea(55.3, 22.1);
+      System.out.println(area);
+    }
+
+    int triangleArea(double base, double height) {
+      return base + height;
+    }
+}
