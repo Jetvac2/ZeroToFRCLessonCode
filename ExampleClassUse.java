49181d5b4d6a3d5ed26703581cfe4b436d37bc18
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-10-20 10:37:02 -0400

    Update ExampleClassUse.java

--- ExampleClassUse.java
+++ ExampleClassUse.java
@@ -1,6 +1,20 @@
-import ExampleClass; 
 class Main {
+  // Decloration
+  ExampleClass example; 
+  
   public static void main(String[] args) {
+    // Definition, //call to the constructor
+    example = new ExampleClass(5);
+    // You can also do this
+    // A variable of a class is called a object
+    // Thus example2 is a object
+    ExampleClass example2 = new ExampleClass(33);
+
+    // Quiz: What will this print?
+    // To call a method from a class you put the name of your object before the method name and then a "." followed by the method call
+    System.out.println(example.getIsExample());
     
+    int sum = example.calcSum(33, 22);
+    System.out.println(sum);
   }
 }
