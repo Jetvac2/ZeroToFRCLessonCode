de18affabd061ee2111372f49bcd1185c1ef0126
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-10-20 10:54:08 -0400

    Create ClassSolutionPart2.java

---
+++ ClassSolutionPart2.java
@@ -0,0 +1,11 @@
+class Main {
+
+  public static void main(String[] args) {
+    Book myBook = new Book("Booky", "Me");
+    System.out.println(myBook.getAuthor);
+    System.out.println(myBook.getTitle);
+
+    myBook.setTitle("Booky Volume 2");
+    System.out.println(myBook.getTitle);
+  }
+}
