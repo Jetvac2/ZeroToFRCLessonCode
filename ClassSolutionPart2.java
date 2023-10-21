6fb411e0af42dd0efae81b56d4eefcc1bc7698be
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-10-20 20:58:58 -0400

    Update ClassSolutionPart2.java

--- ClassSolutionPart2.java
+++ ClassSolutionPart2.java
@@ -2,10 +2,10 @@ class Main {
 
   public static void main(String[] args) {
     Book myBook = new Book("Booky", "Me");
-    System.out.println(myBook.getAuthor);
-    System.out.println(myBook.getTitle);
+    System.out.println(myBook.getAuthor());
+    System.out.println(myBook.getTitle());
 
     myBook.setTitle("Booky Volume 2");
-    System.out.println(myBook.getTitle);
+    System.out.println(myBook.getTitle());
   }
 }
